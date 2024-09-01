cff5aa9e6c966969e15b593c6296ec6ff4ceb02f
Dan Li <danli091981@gmail.com>
2024-09-01 01:15:07 -0400

    Update ForecastByZipCodeRequest.kt

--- Java/WeatherApp/app/src/main/java/com/example/weatherapp/data/server/ForecastByZipCodeRequest.kt
+++ Java/WeatherApp/app/src/main/java/com/example/weatherapp/data/server/ForecastByZipCodeRequest.kt
@@ -6,7 +6,7 @@ import java.net.URL
 class ForecastByZipCodeRequest(private val zipCode: Long, val gson: Gson = Gson()) {
 
     companion object {
-        private const val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+        private const val APP_ID = "abc123xyz"
         private const val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
         private const val COMPLETE_URL = "$URL&APPID=$APP_ID&zip="
     }
